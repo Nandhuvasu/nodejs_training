50b08c3d96da4b99544ee1ef667945f4b13aba5e
Nandhuvasu <nandhinivasudevan93@gmail.com>
2020-05-26 10:19:58 +0530

    Added html to render

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const http = require('http');
 const express = require('express');
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express();
-//listener gets called for every incoming req
-const server = http.createServer(routes.handler);
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+// add a parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes)
+//listener gets called for every incoming req
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, 'views','404.html'))
+})
+app.listen(3000)
---
+++ util/path.js
@@ -0,0 +1,3 @@
+const path = require('path')
+// get main module file path
+module.exports = path.dirname(process.mainModule.filename);
